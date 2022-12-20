f4b17e58a6159b7353a52bd60009e960ea95d051
imauice <102501547+imauice@users.noreply.github.com>
2022-12-21 05:03:44 +0700

    deleted:    api.js 	modified:   app/app.module.js 	modified:   app/index.html 	modified:   app/public/styles/style.css

--- app/app.module.js
+++ app/app.module.js
@@ -1,77 +1,129 @@
 angular.module('App', [])
-  .controller('AppController', function() {
-    var machine = this;
+    .controller('AppController', function ($http) {
+        var machine = this;
 
-    // machine product
-    machine.products = [
-      {
-        id:'no.1',
-        name:'coke',
-        price:12,
-        stock:50
-    },
-    {
-        id:'no.2',
-        name:'coke',
-        price:15,
-        stock:50
-    },
-    {
-        id:'no.3',
-        name:'coke',
-        price:20,
-        stock:50
-    },
+        // machine product
 
-    ];
+        // for api
 
-// machine pad
-machine.pads =[
-    {
-        id:'1bath',
-        price:1
-    },
-    {
-        id:'5bath',
-        price:5
-    },
-    {
-        id:'10bath',
-        price:10
-    },
-]
+        // $http({
+        //     method: 'GET',
+        //     url: '/apiUrl' //some api url
+        //   }).then(res =>{
+        //     console.log(res.data);
+        //     machine.products=res.data; //<-- use this when connect to api
+        //   }
+        //   );
 
-machine.screen= `walcome please select order and then put coin`;
+        // with mockup
+        machine.products = [   //<-- we delete all this when use api
+            {
+                id: 'no.1',
+                name: 'Coke',
+                price: 12,
+                stock: 50
+            },
+            {
+                id: 'no.2',
+                name: 'Fanta',
+                price: 15,
+                stock: 50
+            },
+            {
+                id: 'no.3',
+                name: 'Pepsi',
+                price: 20,
+                stock: 50
+            },
 
- //machine methode
- 
-    machine.addstock = function() {
-      machine.products.push({stock:50});
-    };
+        ];
 
-    var selectProductPrice;
-    var selectProductName;
-    machine.selectproduct = function(price,productname){
-        selectProductPrice = price;
-        selectProductName = productname;
-        machine.screen=`Please put coin ${selectProductPrice} baht` ;
-        return selectProductPrice;
-    }
-    
-    machine.putcoin = function(coin){
+        // machine pad
+        machine.pads = [
+            {
+                id: '1bath',
+                price: 1
+            },
+            {
+                id: '5bath',
+                price: 5
+            },
+            {
+                id: '10bath',
+                price: 10
+            },
+        ]
 
-         if(selectProductPrice>0){
-           selectProductPrice -= coin; 
-           machine.screen =`Remainding ${selectProductPrice}`;
-           if(selectProductPrice==0){
-            machine.screen = 'buy complete';
-            alert(`you buy ${selectProductName} complete`);
-           }
+        machine.screen = `Welcome please select order and then put coin`;
+        machine.oncoin = 0;
+
+        //machine methode
+
+        machine.addstock = function () {
+            machine.products.push({ stock: 50 });
+        };
+
+        var selectProductPrice;
+        var selectProductName;
+        var selectProductStock;
+        var selectProductId;
+
+        machine.updatestock = function(id,remainding){
+           var objIndex =  machine.products.findIndex((obj => obj.id == id));
+           machine.products[objIndex].stock = remainding;
+        };
+     
+
+        machine.selectproduct = function (id,price, productname,stock) {
+            if(machine.oncoin==0){
+                console.log('stock: ',stock);
+                selectProductId = id;
+                selectProductPrice = price;
+                selectProductName = productname;
+                selectProductStock = stock;
+                console.log('id ',selectProductId,' stock ',selectProductStock);
+                
+                machine.screen = `Please put coin ${selectProductPrice} baht`;
+            }
+            else{
+                machine.screen = `Cannot change product after to insert coin remaining is ${selectProductPrice} baht`
+            }
+            return selectProductPrice;
         }
-        else{
-            machine.screen ='please select product';
-            console.log(`error:please select product`);
+
+        machine.putcoin = function (coin) {
+
+            if (selectProductPrice > 0) {
+
+                if(coin<=selectProductPrice){
+
+                    selectProductPrice -= coin;
+                    machine.oncoin = 1;
+                    machine.screen = `Remaining ${selectProductPrice}`;
+                    if(selectProductPrice==0){
+                        const remaindingStock = (selectProductStock-1)
+                        machine.updatestock(selectProductId,remaindingStock)
+                        machine.screen = `buy ${selectProductName} complete`;
+                        machine.oncoin = 0;
+                    }
+                    return;
+                };
+
+                if(coin>selectProductPrice){
+                    console.log("coin ",coin,"Remaining ",selectProductPrice);
+                    machine.screen = `Coin has return to you please insert less than remaining price ${selectProductPrice} baht`;
+                    return;
+                };
+
+               
+            }
+
+            else {
+                machine.screen = 'Please select product';
+                console.log(`Error:please select product`);
+            }
         }
-    }
-   
-  });
+
+        console.log('coining',machine.oncoin);
+
+    });
